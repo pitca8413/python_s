96bc61fd67a2502c79c9a9d442ef663b0547cc39
pitca8413 <pitca8413@gmail.com>
2020-02-22 15:59:06 +0900

    덧셈 퀴즈 앱

---
+++ app7.py
@@ -0,0 +1,18 @@
+# BMI 측정 앱
+# app7.py
+
+import tkinter as tk
+
+# 윈도우 만들기(200x100)
+root = tk.Tk()
+root.geometry("200x100")
+
+# 변수 선언(키, 몸무게, BMI)
+height = tk.StringVar()
+weight = tk.StringVar()
+
+# BMI 결과 출력 함수(disp_label)
+
+# 컨트롤 생성
+
+# 윈도우 컨트롤 배치
