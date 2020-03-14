c66c35bf4267079628ebbb8ca208f0999ec3d2b9
pitca8413 <pitca8413@gmail.com>
2020-03-14 15:05:30 +0900

    이미지 불러와서 출력하기

---
+++ dispimg2.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import tkinter.filedialog as fd
+import PIL.Image
+import PIL.ImageTk
+
+def dispPhoto(path):
+    newImage = PIL.Image.open(path).resize((200, 200))
+
+    # 이미지 레이블에 출력하기
+    imageData = PIL.ImageTk.PhotoImage(newImage)
+    imageLabel.configure(image=imageData)
+    imageLabel.image = imageData
+
+    # 파일 레이블에 경로 출력하기
+    fpathLabel.configure(text=path)
+
+def openFile():
+    fpath = fd.askopenfilename()
+
+    if fpath:
+        dispPhoto(fpath)
+
+root = tk.Tk()
+root.geometry("400x350")
+
+btn = tk.Button(text="파일열기", command=openFile)
+imageLabel = tk.Label()
+fpathLabel = tk.Label()
+btn.pack()
+imageLabel.pack()
+fpathLabel.pack()
+tk.mainloop()
